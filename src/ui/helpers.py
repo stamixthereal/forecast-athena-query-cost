8b3bb2eefb76fac2bd430e7da87d3a6a60f40189
stamixthereal <staskazanov@gmail.com>
2024-05-21 16:50:44 +0200

    Updated UI and small changes in functionality

---
+++ src/ui/helpers.py
@@ -0,0 +1,63 @@
+import os
+import shutil
+import subprocess
+import streamlit as st
+
+def clean_pycache():
+    for root, dirs, files in os.walk(".", topdown=True):
+        for directory in dirs:
+            if directory == "__pycache__":
+                shutil.rmtree(os.path.join(root, directory))
+
+def clean_processed_data():
+    processed_data_dir = "data/processed"
+    for filename in os.listdir(processed_data_dir):
+        if filename != ".gitkeep":
+            os.remove(os.path.join(processed_data_dir, filename))
+
+def clean_raw_data():
+    raw_data_dir = "data/raw"
+    for filename in os.listdir(raw_data_dir):
+        if filename != ".gitkeep":
+            os.remove(os.path.join(raw_data_dir, filename))
+
+def clean_ml_model():
+    ml_model_dir = "src/model"
+    for filename in os.listdir(ml_model_dir):
+        if filename != ".gitkeep":
+            os.remove(os.path.join(ml_model_dir, filename))
+
+def clean_python_cache():
+    shutil.rmtree(".pytest_cache", ignore_errors=True)
+    shutil.rmtree(".ruff_cache", ignore_errors=True)
+
+def clean_docker_resources():
+    subprocess.run(["docker", "stop"] + subprocess.getoutput("docker ps -q").split(), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(["docker", "rm"] + subprocess.getoutput("docker ps -a -q").split(), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(["docker", "rmi"] + subprocess.getoutput("docker images -q").split(), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(["docker", "image", "prune", "-a", "-f"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(["docker", "container", "prune", "-f"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(["docker", "volume", "prune", "-f"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(["docker", "system", "prune", "-f"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+def clean_up_resources():
+    clean_pycache()
+    clean_processed_data()
+    clean_raw_data()
+    clean_ml_model()
+    clean_python_cache()
+    clean_docker_resources()
+
+def run_script_and_display_output(script_path):
+    with st.spinner("Operation in progress. Please wait..."):
+        env = os.environ.copy()
+        env['PYTHONPATH'] = f"{os.getcwd()}"
+        result = subprocess.run(["python", script_path], capture_output=True, text=True, env=env)
+        stdout, stderr = result.stdout, result.stderr
+        if stdout:
+            st.success("Operation completed successfully")
+            with st.expander("Output results:"):
+                st.text(stdout)
+        if stderr:
+            with st.expander("Logs for the job:"):
+                st.text(stderr)
--- src/ui/main.py
+++ src/ui/main.py
@@ -1,7 +1,6 @@
-import subprocess
 import streamlit as st
 import pandas as pd
-import os
+from helpers import clean_up_resources, run_script_and_display_output
 
 st.title("Forecast AWS Athena Query Application")
 
@@ -10,27 +9,14 @@ process_button = st.button("Process Athena Executions")
 transform_button = st.button("Transform Query Logs")
 pipeline_button = st.button("Run The Whole Pipeline")
 prediction_button = st.button("Make Prediction")
-
-def run_script_and_display_output(script_path):
-    with st.spinner("Operation in progress. Please wait..."):
-        env = os.environ.copy()
-        env['PYTHONPATH'] = f"{os.getcwd()}"
-        result = subprocess.run(["python", script_path], capture_output=True, text=True, env=env)
-        stdout = result.stdout
-        stderr = result.stderr
-        if stdout:
-            st.success("Operation completed successfully")
-            st.text(stdout)
-        if stderr:
-            st.info("Logs for the job:")
-            st.text(stderr)
+clean_button = st.button("Clear All Local Cache", type='primary')
 
 if process_button:
     run_script_and_display_output('src/app/parse_athena_executions.py')
 
 elif transform_button:
-    run_script_and_display_output('src/app/transform_query_logs.py')
     st.write("## Result Dataframe")
+    run_script_and_display_output('src/app/transform_query_logs.py')
     df = pd.read_csv('./data/processed/processed_data.csv')
     st.dataframe(df)
 
@@ -45,3 +31,7 @@ elif pipeline_button:
 
 elif prediction_button:
     run_script_and_display_output('src/app/prediction.py')
+
+elif clean_button:
+    clean_up_resources()
+    st.success("Operation completed successfully")
